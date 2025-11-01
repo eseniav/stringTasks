f4638054340db952c4d5ffedb258097c5f391008
eseniav <esenia-v@yandex.ru>
2025-11-01 19:20:51 +0300

    Initial commit of StringTasks project

---
+++ StringTasks/StringTasks.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "test";
+    cout << s << endl;
+    s += " another string";
+    cout << s << endl;
+
+    char str[100] = "test\0";
+    cout << str << endl;
+    strcat(str, " another string");
+    cout << str << endl;
+}
